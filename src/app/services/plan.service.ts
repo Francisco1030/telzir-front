434dd9c481c4040b8f288b953e99ddfd8d66f412
Francisco Viana <franciscoviana862@gmail.com>
2020-02-24 15:30:35 -0300

    created: method findAll in plan.service;

--- src/app/services/plan.service.ts
+++ src/app/services/plan.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+import { Plan } from '../models/plan';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<Plan[]> {
+    const url = `${environment.apiUrl}/plans`;
+    return this.http.get<Plan[]>(url);
+  }
+
 }
