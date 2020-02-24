8708a4023757893f6f55aa5eb760f6a5f72739f1
Francisco Viana <franciscoviana862@gmail.com>
2020-02-24 15:37:08 -0300

    updated: app.component and app-routing;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormCallComponent } from './pages/form-call/form-call.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FormCallComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
