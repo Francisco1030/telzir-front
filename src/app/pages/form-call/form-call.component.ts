100681c27e1e588e626588f1c1f9f3550892ee8e
Francisco Viana <franciscoviana862@gmail.com>
2020-02-24 16:55:26 -0300

    connection with api;

--- src/app/pages/form-call/form-call.component.ts
+++ src/app/pages/form-call/form-call.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PlanService } from 'src/app/services/plan.service';
+import { Plan } from 'src/app/models/plan';
+
 @Component({
   selector: 'app-form-call',
   templateUrl: './form-call.component.html',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormCallComponent implements OnInit {
 
-  constructor() { }
+  plans: Plan[];
 
-  ngOnInit() {
+  constructor(private planService: PlanService) {
+    this.getAllPlans();
+  }
+
+  ngOnInit() {}
+
+  getAllPlans() {
+    this.planService.findAll().subscribe((plans: Plan[]) => {
+      this.plans = plans;
+    });
   }
 
 }
