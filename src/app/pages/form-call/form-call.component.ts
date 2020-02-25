e76cd181d9033ce1b942b312bc7fb426e16668ab
Francisco Viana <franciscoviana862@gmail.com>
2020-02-25 07:31:01 -0300

    updated form;

--- src/app/pages/form-call/form-call.component.ts
+++ src/app/pages/form-call/form-call.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 import { PlanService } from 'src/app/services/plan.service';
 import { Plan } from 'src/app/models/plan';
+import { Call } from 'src/app/models/call';
 
 @Component({
   selector: 'app-form-call',
@@ -9,19 +11,43 @@ import { Plan } from 'src/app/models/plan';
   styleUrls: ['./form-call.component.css']
 })
 export class FormCallComponent implements OnInit {
+  @Input() call: Call = <Call>{};
+  @Output() outputCall: EventEmitter<Call> = new EventEmitter();
 
   plans: Plan[];
+  form: FormGroup;
 
-  constructor(private planService: PlanService) {
+  constructor(private planService: PlanService, private formBuilder: FormBuilder) {
     this.getAllPlans();
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    // this.form = new FormGroup({
+    //   source: new FormControl(null),
+    //   destiny: new FormControl(null),
+    //   time: new FormControl(null),
+    //   plan: new FormControl(null),
+    // });
+
+    this.form = this.formBuilder.group({
+      source: ['Selecione'],
+      destiny: ['Selecione'],
+      time: [null],
+      plan: ['Selecione'],
+    });
+
+    console.log(this.form.value);
+  }
 
   getAllPlans() {
     this.planService.findAll().subscribe((plans: Plan[]) => {
       this.plans = plans;
     });
+  }
+
+  onSubmit() {
+    console.log(this.call);
   }
 
 }
